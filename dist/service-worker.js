8f180c97c469cf88f866a6abded42102b0067603
Norihito Nishioka <abigail4649@gmail.com>
2016-01-30 17:56:47 +0900

    beautify

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -2,16 +2,14 @@
 self.addEventListener('push', function(event) {
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
-    // Set other parameters such as the notification language, a vibration pattern associated
-    // to the notification, an image to show near the body.
     // There are many other possible options, for an exhaustive list see the specs:
     //   https://notifications.spec.whatwg.org/
     self.registration.showNotification('Push TEST', {
-      lang: 'ja-JP',
+      lang: 'ja',
       body: '……きこえますか…きこえますか…Push通知のテストです…今…あなたの…心に…直接…Pushしています…Pushができるのは…Chromeだけでは…ありません…Firefoxを…使うのです…',
+      tag: 'notification',
       icon: 'caesar.jpg',
-      vibrate: [500, 100, 500],
+      vibrate: [500, 100, 500]
     })
   );
 });
